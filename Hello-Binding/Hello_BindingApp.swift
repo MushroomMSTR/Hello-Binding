934ebd5a2b3ed9f6af1a4af0df25edea0b7ebb3c
NazarStf <hworker98@gmail.com>
2023-06-02 12:57:45 +0300

    Initial Commit

---
+++ Hello-Binding/Hello_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_BindingApp.swift
+//  Hello-Binding
+//
+//  Created by NazarStf on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hello_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
