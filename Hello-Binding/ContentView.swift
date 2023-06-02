b35631c9a74ae43d56cf0d169f2419f9c0599e37
NazarStf <hworker98@gmail.com>
2023-06-02 14:57:22 +0300

    all errors have been eliminated, the code has become shorter

--- Hello-Binding/ContentView.swift
+++ Hello-Binding/ContentView.swift
@@ -8,25 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var selectedPage: Int? = nil
-	
 	var body: some View {
 		NavigationView {
 			List {
-				NavigationLink(destination: SpicyList(), tag: 1, selection: $selectedPage) {
-					Text("Spicy List") // 'init(destination:tag:selection:label:)' was deprecated in iOS 16.0: use NavigationLink(value:label:) inside a List within a NavigationStack or NavigationSplitView
+				NavigationLink(destination: SpicyList()) {
+					Text("Spicy List")
 				}
 				// Add more navigation links for other pages
 				
 			}
 			.listStyle(PlainListStyle())
-			.onChange(of: selectedPage) { newValue in
-				if newValue == 1 {
-					// Handle selection for Spicy List page
-				}
-				// Add more cases for other pages
-			}
-			.navigationBarTitle("Hello Binding")
+			.navigationBarTitle("Section 5")
 		}
 	}
 }
